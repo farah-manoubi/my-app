acfbaf5b59d5648f7625ebb38ec6d615a9e61770
farah <farah.manoubi@hotmail.com>
2023-07-27 10:21:40 +0200

    error page done

--- src/pages/not_found/ErrorPage.js
+++ src/pages/not_found/ErrorPage.js
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import style from "./errorPage.module.scss";
+import {Header} from "../../components/header/Header";
+import {Footer} from "../../components/footer/Footer"
+
 export const ErrorPage = () =>{
     return(
-        <h1> NOT FOUNDDD </h1>
+        <>
+            <Header />
+            <div className={style.containerError}>
+                <p id={style.number}>404</p>
+                <div id={style.paragraphError}><p>Oups! La page que vous avez demandé n'existe pas.</p></div>
+            </div>
+            <div className={style.link}><Link to ="/">Retourner à la page d'accueil</Link></div>
+            <Footer />
+        </>
     )
 }
