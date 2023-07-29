8131cdec727843693586a92e877d062679513dcf
farah <farah.manoubi@hotmail.com>
2023-07-29 13:31:45 +0200

    MAJ

--- src/pages/logement/Logement.js
+++ src/pages/logement/Logement.js
@@ -1,18 +1,30 @@
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 
 export const Logement = () =>{
 
+    const [ data , setData ] = useState([]); 
+    const {id} = useParams();
+    
+
+    useEffect(() => {
+        const url = "../data/jsonFile.json";
+        const fetchData = async () => {
+            
+            const response = await fetch(url);
+            const json = await response.json();
+            const result = json.find(item => item.id === id)
+            //setData(json); 
+            console.log(result) 
+        };
+        fetchData();
+    }, [id]);
     
     return(
         <>
-            <h1> Page qui affiche les apparts</h1>
             
-            
-           
-    
-           
         </>
            
         
